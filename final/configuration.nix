9fd351505999e6f99355541293ca473e7428c3e9
Florian Schubert <30634234+JamesofScout@users.noreply.github.com>
2024-05-31 11:25:45 +0200

    Config

--- final/configuration.nix
+++ final/configuration.nix
@@ -53,8 +53,6 @@
       "/var/lib/bluetooth"
       "/var/lib/nixos"
       "/var/lib/systemd/coredump"
-      "/etc/NetworkManager/system-connections"
-      { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
     ];
     files = [
       "/etc/machine-id"
